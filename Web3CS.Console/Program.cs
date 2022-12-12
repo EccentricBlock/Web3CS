f5d2ae71f69308496afa9001d39f7fe7eed60192
EccentricBlock <chris.thomas@devnulled.co.uk>
2022-12-12 17:37:28 +0000

    reminder

--- Web3CS.Console/Program.cs
+++ Web3CS.Console/Program.cs
@@ -47,9 +47,12 @@ Console.WriteLine($"");
 Console.WriteLine($"General/Utility Information:");
 Console.WriteLine($"\t+ Encode Keccak Data: '{await web3Client.GenerateKeccakStringAsync("0x68656c6c6f20776f726c64")}'");
 
+/* METIS Nodes Do Not Support This RPC Method
+ * TODO: Investgate further.
+ * 
 string signedTX = await web3Client.SignTransactionAsync("0xb60e8dd61c5d32be8058bb8eb970870f07233155", "0xd46e8dd67c5d32be8058bb8eb970870f07244567", new HexBigInteger("0x76c0"), new HexBigInteger("0x9184e72a000"), new HexBigInteger("0x9184e72a"), new HexBigInteger("0x76c0"), "0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675");
 Console.WriteLine($"Sign Transaction: '{signedTX}'");       
-
+*/
 
 //https://bytemeta.vip/repo/ethereum-optimism/optimism/issues/1218
 //testing this on Metis which is an Optimism L2 fork
